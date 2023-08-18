7dfe71d06c08886af855fed2736933b2b195ee8b
jmane1121 <soto.quintana.josemanuel@gmail.com>
2023-08-17 21:26:28 -0600

    codigo para LyA

---
+++ Lenguaje_y_automatas.java
@@ -0,0 +1,9 @@
+package com.mycompany.lenguaje_y_automatas;
+public class Lenguaje_y_automatas {
+    public static void main(String[] args) {
+        double hola = 30.1416;
+        float adios = (float)(hola);
+        int numero = 'A';
+        float otroNumero = (float) (numero+35/7-(hola+adios));
+    }
+}
